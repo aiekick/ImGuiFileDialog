6583f9abe7d466497a0597f63559e68ef0d30e74
Alastair Growcott <AlastairGrowcott@yahoo.co.uk>
2019-08-17 07:55:20 +0100

    Changes to get it to work on Linux. This will probably break Windows.

--- ImGuiFileDialog.h
+++ ImGuiFileDialog.h
@@ -1,10 +1,15 @@
+// From https://github.com/aiekick/ImGuiFileDialog
+//
+// Modified by AlastairGrowcott@yahoo.com to work on Posix systems.
+
+
 #ifndef __IMGUI_FILE_DIALOG_H_
 #define __IMGUI_FILE_DIALOG_H_
 
 #include <vector>
 #include <string>
 
-#define MAX_FILE_DIALOG_NAME_BUFFER 1024
+#define MAX_FILE_DIALOG_NAME_BUFFER  (1024u)
 
 struct FileInfoStruct
 {
@@ -16,6 +21,30 @@ struct FileInfoStruct
 
 class ImGuiFileDialog
 {
+public:
+	ImGuiFileDialog();
+	~ImGuiFileDialog();
+
+	static ImGuiFileDialog* Instance()
+	{
+		static ImGuiFileDialog *_instance = new ImGuiFileDialog();
+		return _instance;
+	}
+
+        // The filters are a null-terminator separated and terminated list of file extensions. Each filter should
+        // start with a period. E.g.:
+        //     ".txt\0.c\0.cpp\0.h\0"
+	bool FileDialog(const char* vName, const char* vFilters = 0, std::string vPath = ".", std::string vDefaultFileName = "");
+        void Reset();
+	std::string GetFilepathName();
+	std::string GetCurrentPath();
+	std::string GetCurrentFileName();
+	std::string GetCurrentFilter();
+
+        static char FileNameBuffer[MAX_FILE_DIALOG_NAME_BUFFER];
+	static int FilterIndex;
+	bool IsOk;
+
 private:
 	std::vector<FileInfoStruct> m_FileList;
 	std::string m_SelectedFileName;
@@ -23,29 +52,6 @@ private:
 	std::vector<std::string> m_CurrentPath_Decomposition;
 	std::string m_CurrentFilterExt;
 
-public:
-	static char FileNameBuffer[MAX_FILE_DIALOG_NAME_BUFFER];
-	static int FilterIndex;
-	bool IsOk;
-
-public:
-	static ImGuiFileDialog* Instance()
-	{
-		static ImGuiFileDialog *_instance = new ImGuiFileDialog();
-		return _instance;
-	}
-
-public:
-	ImGuiFileDialog();
-	~ImGuiFileDialog();
-
-	bool FileDialog(const char* vName, const char* vFilters = 0, std::string vPath = ".", std::string vDefaultFileName = "");
-	std::string GetFilepathName();
-	std::string GetCurrentPath();
-	std::string GetCurrentFileName();
-	std::string GetCurrentFilter();
-
-private:
 	void ScanDir(std::string vPath);
 	void SetCurrentDir(std::string vPath);
 	void ComposeNewPath(std::vector<std::string>::iterator vIter);
@@ -53,3 +59,4 @@ private:
 
 
 #endif // __IMGUI_FILE_DIALOG_H_
+
