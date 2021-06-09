b71266d4cdf127b451eac9e5032eb0f7b69073d5
jvn <jvnugteren@protonmail.ch>
2021-06-09 17:01:26 +0200

    Fixed graphical glitch when single-clicking items.

--- ImGuiFileDialog.cpp
+++ ImGuiFileDialog.cpp
@@ -1510,13 +1510,12 @@ namespace IGFD
 				if (ImGui::IsMouseDoubleClicked(0)) // 0 -> left mouse button double click
 				{
 					m_PathClicked = SelectDirectory(vInfos); 
+					return true; // needToBreakTheloop
 				}
 				else if (dlg_filters.empty()) // directory chooser
 				{
 					SelectFileName(vInfos);
 				}
-
-				return true; // needToBreakTheloop
 			}
 			else
 			{
