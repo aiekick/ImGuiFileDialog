bc5e41ffa6d550be5b6ef862e341b02073957485
aiekick <aiekick@funparadigm.com>
2021-09-21 20:17:00 +0200

    [INC] : inc version number

--- ImGuiFileDialog.h
+++ ImGuiFileDialog.h
@@ -479,7 +479,7 @@ ImGuiFontStudio is using also ImGuiFileDialog.
 #ifndef IMGUIFILEDIALOG_H
 #define IMGUIFILEDIALOG_H
 
-#define IMGUIFILEDIALOG_VERSION "v0.6"
+#define IMGUIFILEDIALOG_VERSION "v0.6.1"
 
 #ifndef CUSTOM_IMGUIFILEDIALOG_CONFIG
 #include "ImGuiFileDialogConfig.h"
