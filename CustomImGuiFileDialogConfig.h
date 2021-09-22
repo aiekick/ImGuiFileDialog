0dc650ca45914fdf2eddc7d9b9b2416d28a8da1b
Aiekick <aiekick@funparadigm.com>
2021-09-23 01:15:40 +0200

    Update CustomImGuiFileDialogConfig.h

--- CustomImGuiFileDialogConfig.h
+++ CustomImGuiFileDialogConfig.h
@@ -6,7 +6,8 @@
 // uncomment and modify defines under for customize ImGuiFileDialog
 
 //this options need c++17
-#define USE_STD_FILESYSTEM
+// in this app its defined in CMakeLists.txt
+//#define USE_STD_FILESYSTEM
  
 //#define MAX_FILE_DIALOG_NAME_BUFFER 1024
 //#define MAX_PATH_BUFFER_SIZE 1024
