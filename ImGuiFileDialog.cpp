a84c90a3a6793b88582a862846345d37879d6396
Samuel Venable <samuelvenable@hotmail.com>
2021-12-22 06:34:58 -0800

    Update ImGuiFileDialog.cpp

--- ImGuiFileDialog.cpp
+++ ImGuiFileDialog.cpp
@@ -2928,7 +2928,7 @@ namespace IGFD
 
 			if (g.NavId && g.NavId == vListViewID)
 			{
-				if (ImGui::IsKeyPressedMap(IGFD_KEY_ENTER) ||
+				if (ImGui::IsKeyPressedMap(ImGuiKey_Enter) ||
 					ImGui::IsKeyPressedMap(ImGuiKey_KeyPadEnter) ||
 					ImGui::IsKeyPressedMap(ImGuiKey_Space))
 				{
