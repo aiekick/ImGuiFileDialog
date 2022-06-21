7822966eadb63b379aed6dd1a0eae91b10737d4a
aiekick <aiekick@funparadigm.com>
2022-06-21 22:10:38 +0200

    -

--- ImGuiFileDialog.h
+++ ImGuiFileDialog.h
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2019-2022 Stephane Cuillerdier (aka aiekick)
+Copyright (c) 2018-2022 Stephane Cuillerdier (aka aiekick)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
