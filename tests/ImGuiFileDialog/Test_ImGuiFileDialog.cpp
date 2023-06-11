a22b289916c79fe0a1741178a432240fb0fd3773
aiekick <aiekick@funparadigm.com>
2023-06-11 14:26:40 +0200

    [FIX] : fix for FileStyle by Extention (#118)

--- tests/ImGuiFileDialog/Test_ImGuiFileDialog.cpp
+++ tests/ImGuiFileDialog/Test_ImGuiFileDialog.cpp
@@ -1,9 +1,15 @@
 #include "Test_ImGuiFileDialog.h"
 #include "Test_FilterManager.h"
 #include "Test_FileManager.h"
+#include "Test_FileStyle.h"
 
 bool Test_ImGuiFileDialog(const std::string& vTest) {
-    if (vTest.find("IGFD_FilterManager") != std::string::npos) return Test_FilterManager(vTest);
-    else if (vTest.find("IGFD_FileManager") != std::string::npos) return Test_FileManager(vTest);
+    if (vTest.find("IGFD_FilterManager") != std::string::npos) {
+        return Test_FilterManager(vTest);
+    } else if (vTest.find("IGFD_FileManager") != std::string::npos) {
+        return Test_FileManager(vTest);
+    } else if (vTest.find("IGFD_FileStyle") != std::string::npos) {
+        return Test_FileStyle(vTest);
+    }
     return true;
 }
