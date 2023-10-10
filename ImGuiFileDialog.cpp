e73077d7948bde71e7a050bfe165bdced03f24f1
aiekick <aiekick@funparadigm.com>
2023-10-10 23:27:58 +0200

    [FIX] : now links are correctly shown on unix platforms [#141)

--- ImGuiFileDialog.cpp
+++ ImGuiFileDialog.cpp
@@ -1829,22 +1829,8 @@ IGFD_API void IGFD::FileManager::ScanDir(const FileDialogInternal& vFileDialogIn
                 switch (ent->d_type) {
                     case DT_DIR: fileType.SetContent(FileType::ContentType::Directory); break;
                     case DT_REG: fileType.SetContent(FileType::ContentType::File); break;
-#if DT_LNK != DT_UNKNOWN
+#if defined(_IGFD_UNIX_) || (DT_LNK != DT_UNKNOWN)
                     case DT_LNK: {
-                        fileType.SetSymLink(true);
-                        fileType.SetContent(FileType::ContentType::LinkToUnknown);  // by default if we can't figure out
-                                                                                    // the target type.
-                        struct stat statInfos = {};
-                        int result = stat((path + PATH_SEP + ent->d_name).c_str(), &statInfos);
-                        if (result == 0) {
-                            if (statInfos.st_mode & S_IFREG) {
-                                fileType.SetContent(FileType::ContentType::File);
-                            } else if (statInfos.st_mode & S_IFDIR) {
-                                fileType.SetContent(FileType::ContentType::Directory);
-                            }
-                        }
-                        break;
-                    }
 #endif
                     case DT_UNKNOWN: {
                         struct stat sb = {};
@@ -3352,8 +3338,8 @@ IGFD_API void IGFD::KeyExplorerFeature::prExploreWithkeys(
     if ((flags & ImGuiSelectableFlags_NoPadWithHalfSpacing) == 0) {
         const float spacing_x = span_all_columns ? 0.0f : style.ItemSpacing.x;
         const float spacing_y = style.ItemSpacing.y;
-        const float spacing_L = IM_FLOOR(spacing_x * 0.50f);
-        const float spacing_U = IM_FLOOR(spacing_y * 0.50f);
+        const float spacing_L = IM_TRUNC(spacing_x * 0.50f);
+        const float spacing_U = IM_TRUNC(spacing_y * 0.50f);
         bb.Min.x -= spacing_L;
         bb.Min.y -= spacing_U;
         bb.Max.x += (spacing_x - spacing_L);
