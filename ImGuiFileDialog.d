773ea315267f6f5e197ca59e153a825c5076f9d9
Som Sinha <somaditya29.s@gmail.com>
2025-12-11 20:52:49 -0330

    ImGuiFileDialog added

---
+++ ImGuiFileDialog.d
@@ -0,0 +1,9 @@
+ImGuiFileDialog/ImGuiFileDialog.o: ImGuiFileDialog/ImGuiFileDialog.cpp \
+  ImGuiFileDialog/ImGuiFileDialog.h \
+  ImGuiFileDialog/ImGuiFileDialogConfig.h src/imgui/imgui.h \
+  src/imgui/imconfig.h src/imgui/imgui_internal.h
+ImGuiFileDialog/ImGuiFileDialog.h:
+ImGuiFileDialog/ImGuiFileDialogConfig.h:
+src/imgui/imgui.h:
+src/imgui/imconfig.h:
+src/imgui/imgui_internal.h:
